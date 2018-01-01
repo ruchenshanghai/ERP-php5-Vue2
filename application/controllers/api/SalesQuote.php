caffe8602a2fe5879f44160577ede6231f069273
濡尘赏海 <996956441@qq.com>
2018-01-01 22:25:33 +0800

    display purchase order detail, sales quote detail, sales order detail ok
    purchase inbound detail,
    sales outbound detail,
    other inbound detail,
    other outbound detail
    
    remote deploy ok

--- application/controllers/api/SalesQuote.php
+++ application/controllers/api/SalesQuote.php
@@ -112,4 +112,133 @@ class SalesQuote extends CI_Controller
 
         return $data;
     }
+
+    public function detail()
+    {
+        $postData = json_decode(file_get_contents("php://input"));
+        $userName = $postData->userName;
+        $password = $postData->password;
+        $fetchConfig = $postData->fetchConfig;
+        $response = new ResponseMessage();
+
+        $user = $this->mysql_model->checkUserPwd($userName, $password);
+        if ($user != null) {
+            $response->status = true;
+            $fetchData = $this->detailQuote($fetchConfig);
+            $response->info = $fetchData;
+        }
+        echo json_encode($response);
+    }
+
+    private function detailQuote($fetchConfig)
+    {
+        $data = null;
+        $result = null;
+
+        $id = $fetchConfig->id;
+
+        $data = $this->data_model->get_sale('and (a.id=' . $id . ")", 1);
+        if (count($data) > 0) {
+            $s = $v = array();
+            $result['id'] = intval($data['id']);
+            $result['buId'] = intval($data['buId']);
+            $result['cLevel'] = 0;
+            $result['contactName'] = $data['contactName'];
+            $result['salesId'] = intval($data['salesId']);
+            $result['date'] = $data['billDate'];
+            $result['billNo'] = $data['billNo'];
+            $result['billType'] = $data['billType'];
+            $result['transType'] = intval($data['transType']);
+            $result['totalQty'] = (float)$data['totalQty'];
+            $result['modifyTime'] = $data['modifyTime'];
+            $result['checked'] = intval($data['checked']);
+            $result['checkName'] = $data['checkName'];
+            $result['disRate'] = (float)$data['disRate'];
+            $result['disAmount'] = (float)$data['disAmount'];
+            $result['amount'] = (float)abs($data['amount']);
+            $result['rpAmount'] = (float)abs($data['rpAmount']);
+            $result['customerFree'] = (float)$data['customerFree'];
+            $result['arrears'] = (float)abs($data['arrears']);
+            $result['userName'] = $data['userName'];
+            $result['status'] = intval($data['checked']) == 1 ? 'view' : 'edit'; //edit
+            $result['totalDiscount'] = (float)$data['totalDiscount'];
+            $result['totalAmount'] = (float)abs($data['totalAmount']);
+            $result['description'] = $data['description'];
+            $result['currency'] = intval($data['currency']);
+            $result['currencyCode'] = $data['currencyCode'];
+            $result['currencyText'] = $data['currencyText'];
+            $result['paymentMethod'] = intval($data['paymentMethod']);
+            $result['shippingMethod'] = intval($data['shippingMethod']);
+            if (!empty($data['deliveryDate'])) {
+                try {
+                    if ($data['deliveryDate'] != '0000-00-00') {
+                        $datetime1 = date_create($data['deliveryDate']);
+                        $datetime2 = date_create($data['billDate']);
+                        $interval = date_diff($datetime1, $datetime2);
+                        $result['deliveryDate'] = $interval->days;
+                    } else {
+                        $data['deliveryDate'] = "";
+                    }
+                } catch (Exception $e) {
+                }
+            }
+            $list = $this->data_model->get_sale_info('and (iid=' . $id . ') order by id');
+            foreach ($list as $arr => $row) {
+                $v[$arr]['invSpec'] = $row['invSpec'];
+                $v[$arr]['taxRate'] = (float)$row['taxRate'];
+                $v[$arr]['srcEntryId'] = intval($row['srcEntryId']);
+                $v[$arr]['srcBillNo'] = $row['srcBillNo'];
+                $v[$arr]['srcId'] = intval($row['srcId']);
+                $v[$arr]['goods'] = $row['invName'];
+                $v[$arr]['spec'] = $row['invSpec'];
+                $v[$arr]['invName'] = $row['invName'];
+                $v[$arr]['qty'] = (float)abs($row['qty']);
+                $v[$arr]['locationName'] = $row['locationName'];
+                $v[$arr]['amount'] = (float)abs($row['amount']);
+                $v[$arr]['taxAmount'] = (float)$row['taxAmount'];
+                $v[$arr]['price'] = (float)$row['price'];
+                $v[$arr]['tax'] = (float)$row['tax'];
+                $v[$arr]['mainUnit'] = $row['mainUnit'];
+                $v[$arr]['deduction'] = (float)$row['deduction'];
+                $v[$arr]['invId'] = intval($row['invId']);
+                $v[$arr]['invNumber'] = $row['invNumber'];
+                $v[$arr]['locationId'] = intval($row['locationId']);
+                $v[$arr]['locationName'] = $row['locationName'];
+                $v[$arr]['discountRate'] = (float)$row['discountRate'];
+                $v[$arr]['description'] = $row['description'];
+                $v[$arr]['unitId'] = intval($row['unitId']);
+                $v[$arr]['mainUnit'] = $row['mainUnit'];
+
+                // 库存数量
+                // 获取该商品在库数量
+                $res = $this->mysql_model->query(INVOICE_INFO, "SELECT SUM(qty) as stockQty from " . INVOICE_INFO . " WHERE invId=$row[invId]");
+                $v [$arr] ['stockQty'] = ($res ['stockQty'] == null) ? 0 : $res ['stockQty'];
+
+            }
+            $result['entries'] = $v;
+            $result['accId'] = (float)$data['accId'];
+
+            $accounts = $this->data_model->get_account_info('and (iid=' . $id . ') order by id');
+            foreach ($accounts as $arr => $row) {
+                $s[$arr]['invoiceId'] = intval($id);
+                $s[$arr]['billNo'] = $row['billNo'];
+                $s[$arr]['buId'] = intval($row['buId']);
+                $s[$arr]['billType'] = $row['billType'];
+                $s[$arr]['transType'] = $row['transType'];
+                $s[$arr]['transTypeName'] = $row['transTypeName'];
+                $s[$arr]['billDate'] = $row['billDate'];
+                $s[$arr]['accId'] = intval($row['accId']);
+                $s[$arr]['account'] = $row['accountNumber'] . ' ' . $row['accountName'];
+                $s[$arr]['payment'] = (float)abs($row['payment']);
+                $s[$arr]['wayId'] = (float)$row['wayId'];
+                $s[$arr]['way'] = $row['categoryName'];
+                $s[$arr]['settlement'] = $row['settlement'];
+                if ($result['accId'] == $row['accId']) {
+                    $result['account'] = $row['accountName'];
+                }
+            }
+            $result['accounts'] = $s;
+        }
+        return $result;
+    }
 }
